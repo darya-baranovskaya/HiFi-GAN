ab27b909f07fa6f817d8a3b2d6cc3102411655db
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-12-21 02:36:49 +0300

    gan_training

--- gan_training.py
+++ gan_training.py
@@ -6,9 +6,11 @@ from utils.melspectrogram import *
 
 device = torch.device('cuda')
 
+
 def model_require_grad(model, require_grad):
     for param in model.parameters():
         param.require_grad = require_grad
+
 
 class L1MelLoss():
     def __init__(self):
@@ -31,32 +33,29 @@ class L1MelLoss():
 
 class GANTraining():
     def __init__(self, model_gen, model_disc, optimizer_gen, optimizer_disc, scheduler_gen=None, scheduler_disc=None,
-                 lambda_mel=1, lambda_wav=1):
+                 lambda_mel=45, lambda_wav=2, n_gen_steps=1):
         self.model_gen, self.model_disc = model_gen, model_disc
         self.optimizer_gen, self.optimizer_disc = optimizer_gen, optimizer_disc
         self.scheduler_gen, self.scheduler_disc = scheduler_gen, scheduler_disc
-        # self.n_gen_steps = 1
         # self.epoch = 0
-
 
         self.gen_steps = 0
         self.disc_steps = 0
 
-        #losses
+        # losses
         self.lambda_mel = lambda_mel
         self.lambda_wav = lambda_wav
         self.mel_loss = L1MelLoss()
         self.feature_loss = nn.L1Loss()
 
         self.gan_loss = nn.MSELoss()
+        self.n_gen_steps = n_gen_steps
 
     def train_step(self, train_loader):
         self.model_gen.train(True)
         self.model_disc.train(True)
         self.gen_loss = 0
         self.disc_loss = 0
-
-        done_gen_steps = 0
         #         do_disc_step = True
 
         for batch_num, batch in enumerate(train_loader):
@@ -66,15 +65,16 @@ class GANTraining():
             # loss = step_disc(batch)
 
             # backward step
+            #             if self.gen_steps >= self.n_gen_steps:
+            #                 self.gen_steps = 0
             disc_loss = self.step_disc(mel, wav)
+            self.disc_loss += disc_loss.item()
 
             gen_loss = self.step_gen(mel, wav)
 
             #             if do_gen_step:
 
             self.gen_loss += gen_loss.item()
-            self.disc_loss += disc_loss.item()
-
 
             if self.scheduler_disc is not None:
                 self.scheduler_disc.step()
@@ -83,21 +83,20 @@ class GANTraining():
 
         return self.gen_loss / len(train_loader), self.disc_loss / len(train_loader)
 
-
     def step_gen(self, mel, wav):
         self.gen_steps += 1
         model_require_grad(self.model_disc, False)
         model_require_grad(self.model_gen, True)
         self.optimizer_gen.zero_grad()
 
-        pred_wav = self.model_gen(mel).detach()
+        pred_wav = self.model_gen(mel)
         gan_loss, feature_loss, rec_loss = self.calc_gen_loss(mel, wav, pred_wav)
 
         gen_loss = gan_loss + feature_loss + rec_loss
         gen_loss.backward()
         self.optimizer_gen.step()
 
-        wandb.log({'train_gen_gan_loss': gan_loss.item(), 'train_gen_feature_loss':feature_loss.item(),
+        wandb.log({'train_gen_gan_loss': gan_loss.item(), 'train_gen_feature_loss': feature_loss.item(),
                    'train_gen_rec_loss': rec_loss.item(), 'train_gen_loss': gen_loss.item()})
         return gen_loss
 
@@ -115,7 +114,7 @@ class GANTraining():
         disc_loss.backward()
         self.optimizer_disc.step()
 
-        wandb.log({'train_disc_pred_loss': pred_loss.item(), 'train_disc_real_loss':real_loss.item(),
+        wandb.log({'train_disc_pred_loss': pred_loss.item(), 'train_disc_real_loss': real_loss.item(),
                    'train_disc_loss': disc_loss.item()})
 
         return disc_loss
@@ -126,7 +125,6 @@ class GANTraining():
             for pred, real in zip(preds, reals):
                 loss += self.feature_loss(pred, real)
         return loss
-
 
     def calc_gan_loss(self, msds, mpds, is_real):
         loss = 0
@@ -142,7 +140,7 @@ class GANTraining():
             else:
                 target_mpd = torch.zeros_like(mpd)
             loss += self.gan_loss(mpd, target_mpd)
-
+        return loss
 
     def calc_gen_loss(self, mel, wav, pred_wav):
         if wav.shape[-1] < pred_wav.shape[-1]:
@@ -150,7 +148,7 @@ class GANTraining():
         elif wav.shape[-1] > pred_wav.shape[-1]:
             pred_wav = F.pad(pred_wav, (0, wav.shape[-1] - pred_wav.shape[-1]))
 
-        #mpd feature
+        # mpd feature
         disc_mpd_pred, disc_mpd_pred_fm = self.model_disc.mpd(pred_wav.unsqueeze(1))
         disc_mpd_real, disc_mpd_real_fm = self.model_disc.mpd(wav.unsqueeze(1))
 
@@ -166,7 +164,7 @@ class GANTraining():
 
         gan_loss = self.calc_gan_loss(disc_msd_pred, disc_mpd_pred, is_real=True)
 
-        return gan_loss, (msd_feature_loss +  mpd_feature_loss)* self.lambda_wav, mel_loss * self.lambda_mel
+        return gan_loss, (msd_feature_loss + mpd_feature_loss) * self.lambda_wav, mel_loss * self.lambda_mel
 
     def calc_disc_loss(self, mel, wav, pred_wav):
         if wav.shape[-1] < pred_wav.shape[-1]:
@@ -174,7 +172,7 @@ class GANTraining():
         elif wav.shape[-1] > pred_wav.shape[-1]:
             pred_wav = F.pad(pred_wav, (0, wav.shape[-1] - pred_wav.shape[-1]))
 
-        #mpd feature
+        # mpd feature
         disc_mpd_pred, _ = self.model_disc.mpd(pred_wav.unsqueeze(1))
         disc_mpd_real, _ = self.model_disc.mpd(wav.unsqueeze(1))
 
